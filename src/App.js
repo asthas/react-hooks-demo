a44fc03666a0842a66d39945d1cb131e20059e0c
Astha Sharma <a@astha.me>
2020-01-21 10:12:16 -0800

    Wrapped the input handler in useCallback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import useFetch from './useFetch';
 import './App.css';
 
 function App() {
   let [searchTerm, setSearchTerm] = useState('')
+
+  const handleInput = useCallback((e) => {
+    setSearchTerm(e.target.value)
+  }, [])
+
   let articles = useFetch(`https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=${searchTerm}`)
   return(
     <div>
       <input
         type="search"
-        onInput={(e) => setSearchTerm(e.target.value)}
+        onInput={handleInput}
       />
       {articles.map((article,i) => (
         <li key={i}>{article}</li>
