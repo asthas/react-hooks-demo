6dc72b57ef49a92a1f6ff207c028a4f6fbb5b790
Astha Sharma <a@astha.me>
2020-02-10 10:20:34 -0800

    Refactored useFetchAllrequests

--- src/useFetchAllRequests.js
+++ src/useFetchAllRequests.js
@@ -1,24 +1,20 @@
 import { useState, useEffect, useMemo } from 'react';
 
+const defaultData = []
+
 function useFetchAllRequests(urls) {
-
-  const [fetchedData, setFetchedData] = useState([])
-
-  let fetchArr = useMemo(
-    () =>
-      urls.map(function(url) {
-        return fetch(url);
-      }),
-    []
-  )
+  const [fetchedData, setFetchedData] = useState(defaultData)
 
   useEffect(() => {
+    const fetchArr = urls.map(function(url) {
+      return fetch(url);
+    })
     const fetchAllUrls = () => Promise.all(fetchArr).then(function(response){
       const responses = response.map((res) => res.json())
       return Promise.all(responses)
     }).then((data) => setFetchedData(data))
     fetchAllUrls()
-  }, [fetchArr])
+  }, [urls])
 
   return fetchedData
 
